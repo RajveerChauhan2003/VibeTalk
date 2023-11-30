35ac32c739db3da7056f38ecb0019742fb989f8c
Rajveer <rajveersinghchauhan2003@gmail.com>
2023-11-30 19:54:56 +0530

    Added Firebase Dependencies and made some frontend changes

--- app/src/main/java/com/miniproject/vibetalk/LoginActivity.java
+++ app/src/main/java/com/miniproject/vibetalk/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.miniproject.vibetalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,19 +8,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailBox , passwordBox;
     Button loginbtn , signupbtn;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth=FirebaseAuth.getInstance();
         emailBox=findViewById(R.id.emailBox);
         passwordBox=findViewById(R.id.passwordBox);
         loginbtn=findViewById(R.id.loginBtn);
         signupbtn=findViewById(R.id.createBtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email=emailBox.getText().toString();
+                password=passwordBox.getText().toString();
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/miniproject/vibetalk/SignupActivity.java
+++ app/src/main/java/com/miniproject/vibetalk/SignupActivity.java
@@ -1,14 +1,68 @@
 package com.miniproject.vibetalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    EditText emailBox , passwordBox , namebox;
+    Button loginbtn , signupbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        auth=FirebaseAuth.getInstance();
+        namebox=findViewById(R.id.nameBox);
+        emailBox=findViewById(R.id.emailBox);
+        passwordBox=findViewById(R.id.passwordBox);
+        loginbtn=findViewById(R.id.loginBtn);
+        signupbtn=findViewById(R.id.createBtn);
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,pass,name;
+                email=emailBox.getText().toString();
+                pass=passwordBox.getText().toString();
+                name=namebox.getText().toString();
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Account is created", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
